82f7d57d5bdb134dd5e1405aaa55e4e1c8b5ca18
Tiago Royer <royertiago@gmail.com>
2015-04-17 15:36:22 -0300

    cmdline_range.test.cpp: Add extremum value tests.

--- cmdline_range.test.cpp
+++ cmdline_range.test.cpp
@@ -24,6 +24,11 @@ TEST_CASE( "range_parser with integers at beginning", "[range_parser]" ) {
         CHECK( i == 42 );
         CHECK( log.str() == "Error: number must be greater than 50.\n" );
     }
+    SECTION( "Almost outside [min, \\infty)" ) {
+        args.range(42) >> i;
+        CHECK( i == 42 );
+        CHECK( log.str() == "" );
+    }
     SECTION( "Inside [min, max]" ) {
         args.range(40, 50) >> i;
         CHECK( i == 42 );
@@ -38,6 +43,16 @@ TEST_CASE( "range_parser with integers at beginning", "[range_parser]" ) {
         args.range(30, 40) >> i;
         CHECK( i == 42 );
         CHECK( log.str() == "Error: number must be smaller than 40.\n" );
+    }
+    SECTION( "Almost below [min, max]" ) {
+        args.range(42, 60) >> i;
+        CHECK( i == 42 );
+        CHECK( log.str() == "" );
+    }
+    SECTION( "Almost above [min, max]" ) {
+        args.range(30, 42) >> i;
+        CHECK( i == 42 );
+        CHECK( log.str() == "" );
     }
 }
 
@@ -63,6 +78,11 @@ TEST_CASE( "range_parser with integers at middle", "[range_parser]" ) {
         CHECK( i == 42 );
         CHECK( log.str() == "Error: argument to --value must be greater than 50.\n" );
     }
+    SECTION( "Almost outside [min, \\infty)" ) {
+        args.range(42) >> i;
+        CHECK( i == 42 );
+        CHECK( log.str() == "" );
+    }
     SECTION( "Inside [min, max]" ) {
         args.range(40, 50) >> i;
         CHECK( i == 42 );
@@ -77,6 +97,16 @@ TEST_CASE( "range_parser with integers at middle", "[range_parser]" ) {
         args.range(30, 40) >> i;
         CHECK( i == 42 );
         CHECK( log.str() == "Error: argument to --value must be smaller than 40.\n" );
+    }
+    SECTION( "Almost below [min, max]" ) {
+        args.range(42, 60) >> i;
+        CHECK( i == 42 );
+        CHECK( log.str() == "" );
+    }
+    SECTION( "Almost above [min, max]" ) {
+        args.range(30, 42) >> i;
+        CHECK( i == 42 );
+        CHECK( log.str() == "" );
     }
 }
 
@@ -100,6 +130,11 @@ TEST_CASE( "range_parser with double at beginning", "[range_parser]" ) {
         CHECK( d == 42 );
         CHECK( log.str() == "Error: number must be greater than 50.\n" );
     }
+    SECTION( "Almost outside [min, \\infty)" ) {
+        args.range(42) >> d;
+        CHECK( d == 42 );
+        CHECK( log.str() == "" );
+    }
     SECTION( "Inside [min, max]" ) {
         args.range(40, 50) >> d;
         CHECK( d == 42 );
@@ -114,6 +149,16 @@ TEST_CASE( "range_parser with double at beginning", "[range_parser]" ) {
         args.range(30, 40) >> d;
         CHECK( d == 42 );
         CHECK( log.str() == "Error: number must be smaller than 40.\n" );
+    }
+    SECTION( "Almost below [min, max]" ) {
+        args.range(42, 60) >> d;
+        CHECK( d == 42 );
+        CHECK( log.str() == "" );
+    }
+    SECTION( "Almost above [min, max]" ) {
+        args.range(30, 42) >> d;
+        CHECK( d == 42 );
+        CHECK( log.str() == "" );
     }
 }
 
@@ -139,6 +184,11 @@ TEST_CASE( "range_parser with double at middle", "[range_parser]" ) {
         CHECK( d == 42 );
         CHECK( log.str() == "Error: argument to --value must be greater than 50.\n" );
     }
+    SECTION( "Almost outside [min, \\infty)" ) {
+        args.range(42) >> d;
+        CHECK( d == 42 );
+        CHECK( log.str() == "" );
+    }
     SECTION( "Inside [min, max]" ) {
         args.range(40, 50) >> d;
         CHECK( d == 42 );
@@ -153,5 +203,15 @@ TEST_CASE( "range_parser with double at middle", "[range_parser]" ) {
         args.range(30, 40) >> d;
         CHECK( d == 42 );
         CHECK( log.str() == "Error: argument to --value must be smaller than 40.\n" );
+    }
+    SECTION( "Almost below [min, max]" ) {
+        args.range(42, 60) >> d;
+        CHECK( d == 42 );
+        CHECK( log.str() == "" );
+    }
+    SECTION( "Almost above [min, max]" ) {
+        args.range(30, 42) >> d;
+        CHECK( d == 42 );
+        CHECK( log.str() == "" );
     }
 }
